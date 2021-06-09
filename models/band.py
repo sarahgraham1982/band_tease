42ac1b460be042b065e2ea72443a78341fceb5ac
sarahgrahamcoding@hotmail.com <sarahgrahamcoding@hotmail.com>
2021-06-09 15:17:51 +0100

    added models code

---
+++ models/band.py
@@ -0,0 +1,8 @@
+class Band:
+    
+    def __init__(self, band_name, favourite_song, favourite_album, fun_fact, id = None):
+        self.band_name = band_name
+        self.favourite_song = favourite_song
+        self.favourite_album = favourite_album
+        self.fun_fact = fun_fact
+        self.id = id
