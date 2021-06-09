8e8d5b36d4a1c6950bc56741e162a3665926c80d
sarahgrahamcoding@hotmail.com <sarahgrahamcoding@hotmail.com>
2021-06-09 16:59:55 +0100

    added base and index html files to templates

--- controllers/bands_controller.py
+++ controllers/bands_controller.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request
 from flask import Blueprint
-from models.bands import Band
+from models.band import Band
 import repositories.band_repository as band_repository
 import repositories.user_repository as user_repository
 
@@ -20,6 +20,17 @@ def new_band():
 
 @bands_blueprint.route("/bands", methods=['POST'])
 def create_band():
-    
+    band_name           = request.form['band_name']
+    genre               = request.form['genre']
+    favourite_song      = request.form['favourite_song']
+    favourite_album     = request.form['favourtie_album']
+    fun_fact            = request.form['fun_fact']
+    user                = user_repository.select(user_id)
+    band                = Band(band_name, genre, favourite_song, favourite_album, fun_fact)
+    band_repository.save(task)
+    return redirect('/bands') 
+
+
+
 
 
