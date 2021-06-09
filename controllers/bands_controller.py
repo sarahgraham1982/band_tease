14ac0df194eb5f18cdc09afc93eef2f4ee491065
sarahgrahamcoding@hotmail.com <sarahgrahamcoding@hotmail.com>
2021-06-09 17:17:11 +0100

    commit the rest of bands controller

--- controllers/bands_controller.py
+++ controllers/bands_controller.py
@@ -21,14 +21,46 @@ def new_band():
 @bands_blueprint.route("/bands", methods=['POST'])
 def create_band():
     band_name           = request.form['band_name']
+    user_id             = request.form['user_id']
     genre               = request.form['genre']
     favourite_song      = request.form['favourite_song']
     favourite_album     = request.form['favourtie_album']
     fun_fact            = request.form['fun_fact']
     user                = user_repository.select(user_id)
     band                = Band(band_name, genre, favourite_song, favourite_album, fun_fact)
-    band_repository.save(task)
+    band_repository.save(band)
     return redirect('/bands') 
+
+@bands_blueprint.route("/bands/<id>", methods=['GET'])
+def show_band(id):
+    band = band_repository.select(id)
+    return render_template('bands/show.html', band = band)
+
+@bands_blueprint.route("/bands/<id>/edit", methods=['GET"])
+def edit_band(id):
+    band = band_repository.select(id)
+    users = user_repository.select_all()
+    return render_template('bands/edit.html', band = band, all_users = users)
+
+@bands_blueprint.route("/bands/<id>", methods=['POST'])
+def update_band(id):
+    band_name           = request.form['band_name']
+    user_id             = request.form['user_id']
+    genre               = request.form['genre']
+    favourite_song      = request.form['favourite_song']
+    favourite_album     = request.form['favourtie_album']
+    fun_fact            = request.form['fun_fact']
+    user                = user_repository.select(user_id)
+    band                = Band(band_name, genre, favourite_song, favourite_album, fun_fact)
+    band_repository.update(band)
+    return redirect('/bands') 
+
+@bands_blueprint.route("/bands/<id>/delete", methods=['POST'])
+def delete_band(id):
+    band_repository.delete(id)
+    return redirect('/bands')
+
+
 
 
 
