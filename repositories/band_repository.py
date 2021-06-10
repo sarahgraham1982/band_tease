668608c9fcf7fc3b4a21b6d9f3e189ce2ee9ce19
sarahgrahamcoding@hotmail.com <sarahgrahamcoding@hotmail.com>
2021-06-10 12:16:39 +0100

    added functions in band_repository

--- repositories/band_repository.py
+++ repositories/band_repository.py
@@ -6,3 +6,49 @@ import repositories.user_repository as user_repository
 
 
 def save(band):
+    sql = "INSERT INTO bands(band_name, genre, favourite_song, favourite_album, fun_fact, user_id) VALUES (%s, %s, %s, %s, %s, %s) RETURNING *"
+    values = [band.band_name, band.genre, band.favourite_song, band.favourite_album, band.fun_fact, band.user_id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    band.id = id
+    return band
+
+def select_all ():
+    bands = []
+
+    sql = "SELECT * FROM bands"
+    results = run_sql(sql)
+
+    for row in results:
+        user = user_repository.select(row['user_id'])
+        band = Band(row['band_name'], row['genre'], row['favourite_song'], row['favourite_album'], row['fun_fact'], user, row['id'])
+        bands.append(band)
+    return bands
+
+
+def select(id):
+    band = None
+    sql = "SELECT * FROM bands WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        user = user_repository.select(result['user_id'])
+        band = Band(result['band_name'], result['genre'], result['favourite_song'], result['favourite_album'], result['fun_fact'], user, result['id'])
+    return band
+
+
+def delete(id):
+    sql = "DELETE FROM tasks WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def delete_all():
+    sql = "DELETE FROM tasks"
+    run_sql(sql)
+
+def update(band):
+    sql = "UPDATE bands SET (band_name, genre, favourite_song, favourite_album, fun_fact, user_id) = (%s, %s, %s, %s, %s, %s) WHERE id = %s"
+    values = [band.band_name, band.genre, band.favourite_song, band.favourite_album, band.fun_fact, band.user_id]
+    run_sql(sql, values)
