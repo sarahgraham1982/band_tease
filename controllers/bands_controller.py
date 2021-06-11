6e8465b2fad7f8427f858cf51f4ddbc871ff2ec2
sarahgrahamcoding@hotmail.com <sarahgrahamcoding@hotmail.com>
2021-06-11 15:15:32 +0100

    amended restful routes

--- controllers/bands_controller.py
+++ controllers/bands_controller.py
@@ -6,59 +6,65 @@ import repositories.user_repository as user_repository
 
 bands_blueprint = Blueprint("bands", __name__)
 
-@bands_blueprint.route("/bands")
-def bands():
-    bands = band_repository.select_all()
-    return render_template("bands/index.html", all_bands = bands)
+# RESTful routes
+# Landing page
 
-# CRUD
+@bands_blueprint.route("/")
+def home():
+    return render_template("index.html")
 
-@bands_blueprint.route("/bands/new", methods=['GET'])
-def new_band():
-    users = user_depository.select_all()
-    return render_template("bands/new.html", all_users = users)
+# @bands_blueprint.route("/bands")
+# def bands():
+#     bands = band_repository.select_all()
+#     return render_template("bands/index.html", all_bands = bands)
 
-@bands_blueprint.route("/bands", methods=['POST'])
-def create_band():
-    band_name           = request.form['band_name']
-    user_id             = request.form['user_id']
-    genre               = request.form['genre']
-    favourite_song      = request.form['favourite_song']
-    favourite_album     = request.form['favourtie_album']
-    fun_fact            = request.form['fun_fact']
-    user                = user_repository.select(user_id)
-    band                = Band(band_name, genre, favourite_song, favourite_album, fun_fact)
-    band_repository.save(band)
-    return redirect('/bands') 
 
-@bands_blueprint.route("/bands/<id>", methods=['GET'])
-def show_band(id):
-    band = band_repository.select(id)
-    return render_template('bands/show.html', band = band)
+# @bands_blueprint.route("/bands/new", methods=['GET'])
+# def new_band():
+#     users = user_depository.select_all()
+#     return render_template("bands/new.html", all_users = users)
 
-@bands_blueprint.route("/bands/<id>/edit", methods=['GET'])
-def edit_band(id):
-    band = band_repository.select(id)
-    users = user_repository.select_all()
-    return render_template('bands/edit.html', band = band, all_users = users)
+# @bands_blueprint.route("/bands", methods=['POST'])
+# def create_band():
+#     band_name           = request.form['band_name']
+#     user_id             = request.form['user_id']
+#     genre               = request.form['genre']
+#     favourite_song      = request.form['favourite_song']
+#     favourite_album     = request.form['favourtie_album']
+#     fun_fact            = request.form['fun_fact']
+#     user                = user_repository.select(user_id)
+#     band                = Band(band_name, genre, favourite_song, favourite_album, fun_fact)
+#     band_repository.save(band)
+#     return redirect('/bands') 
 
-@bands_blueprint.route("/bands/<id>", methods=['POST'])
-def update_band(id):
-    band_name           = request.form['band_name']
-    user_id             = request.form['user_id']
-    genre               = request.form['genre']
-    favourite_song      = request.form['favourite_song']
-    favourite_album     = request.form['favourtie_album']
-    fun_fact            = request.form['fun_fact']
-    user                = user_repository.select(user_id)
-    band                = Band(band_name, genre, favourite_song, favourite_album, fun_fact)
-    band_repository.update(band)
-    return redirect('/bands') 
+# @bands_blueprint.route("/bands/<id>", methods=['GET'])
+# def show_band(id):
+#     band = band_repository.select(id)
+#     return render_template('bands/show.html', band = band)
 
-@bands_blueprint.route("/bands/<id>/delete", methods=['POST'])
-def delete_band(id):
-    band_repository.delete(id)
-    return redirect('/bands')
+# @bands_blueprint.route("/bands/<id>/edit", methods=['GET'])
+# def edit_band(id):
+#     band = band_repository.select(id)
+#     users = user_repository.select_all()
+#     return render_template('bands/edit.html', band = band, all_users = users)
+
+# @bands_blueprint.route("/bands/<id>", methods=['POST'])
+# def update_band(id):
+#     band_name           = request.form['band_name']
+#     user_id             = request.form['user_id']
+#     genre               = request.form['genre']
+#     favourite_song      = request.form['favourite_song']
+#     favourite_album     = request.form['favourtie_album']
+#     fun_fact            = request.form['fun_fact']
+#     user                = user_repository.select(user_id)
+#     band                = Band(band_name, genre, favourite_song, favourite_album, fun_fact)
+#     band_repository.update(band)
+#     return redirect('/bands') 
+
+# @bands_blueprint.route("/bands/<id>/delete", methods=['POST'])
+# def delete_band(id):
+#     band_repository.delete(id)
+#     return redirect('/bands')
 
 
 
